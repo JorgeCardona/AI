af9e4b3011634342607502cbe5603b82e9737816
JorgeCardona <jorgecardona@utp.edu.co>
2019-06-23 23:07:42 -0500

    update credentials

--- Ecosystems_Project/COMPONENTS/MODEL/db_config.py
+++ Ecosystems_Project/COMPONENTS/MODEL/db_config.py
@@ -1,6 +1,6 @@
 MYSQL_HOST     = 'localhost'
 MYSQL_PORT     = '3306'
 MYSQL_USER     = 'root'
-MYSQL_PASSWORD = '12345678'
+MYSQL_PASSWORD = 'xxxxxx'
 MYSQL_SCHEMA   = 'divergente'
 MYSQL_CHARSET  = 'utf8'
