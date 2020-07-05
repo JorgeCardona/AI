2beccf21d4bc6592f2f895a33efda0ffbef66ef9
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2020-07-04 22:25:07 -0500

    fix

--- API RESTFULL MACHINE LEARNING/UTILS/tables_collections.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from configuration.config import ALL_DATA, PREDICTION, CLASIFICATOR
-
-class StoreData(Enum):
-
-    SAVE_ALL_DATA     = ALL_DATA
-    SAVE_PREDICTION   = PREDICTION
-    SAVE_CLASIFICATOR = CLASIFICATOR
